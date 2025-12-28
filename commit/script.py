e2a3c11b8ab2f8d402c8e44a5f46212b8bd7996d
Viwu <sinconsistencia@gmail.com>
2025-12-27 19:58:50 -0600

    it works now

--- commit/script.py
+++ commit/script.py
@@ -1,5 +1,6 @@
-import subprocess, datetime
-from datetime import *
+import subprocess, datetime, time
+from datetime import date
+from datetime import datetime
 
 now = datetime.now().time()
 today = date.today()
@@ -8,8 +9,15 @@ today = date.today()
 version = f"{today.year + today.month + today.day}.{now.hour + now.minute}"
 print(version)
 
+# git push
 # git tag _
 # git push origin --tags
-subprocess.run(["git", "push"], capture_output=True, text=True)
-subprocess.run(["git", "tag", version], capture_output=True, text=True)
-subprocess.run(["git", "push", "origin" "--tags"], capture_output=True, text=True)
+
+gitpush = subprocess.run(["git", "push"], capture_output=True, text=True)
+print(gitpush.stderr)
+
+gittag = subprocess.run(["git", "tag", version], capture_output=True, text=True)
+gitpushtag = subprocess.run(["git", "push", "origin", "--tags"], capture_output=True, text=True)
+
+print(gittag.stderr)
+print(gitpushtag.stderr)
