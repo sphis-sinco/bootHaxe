6339243d0cad7f2e9c27871df4488ebe72a6c8a5
Viwu <sinconsistencia@gmail.com>
2025-12-27 19:54:35 -0600

    git tag cmds not workin?????

--- commit/script.py
+++ commit/script.py
@@ -10,6 +10,6 @@ print(version)
 
 # git tag _
 # git push origin --tags
-commitfirst = subprocess.run(["git", "push"], capture_output=True, text=True)
-maketag = subprocess.run(["git", "tag", version], capture_output=True, text=True)
-pushtag = subprocess.run(["git", "push", "origin" "--tags"], capture_output=True, text=True)
+subprocess.run(["git", "push"], capture_output=True, text=True)
+subprocess.run(["git", "tag", version], capture_output=True, text=True)
+subprocess.run(["git", "push", "origin" "--tags"], capture_output=True, text=True)
